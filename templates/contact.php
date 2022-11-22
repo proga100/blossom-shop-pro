28604fea444a5dc0eb20d587430f8a0e8dd6c30b
BK <akhmedov@balcomsoft.com>
2022-11-22 18:39:14 +0500

    dd

--- templates/contact.php
+++ templates/contact.php
@@ -8,18 +8,22 @@
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<?php 
-				if( is_active_sidebar( 'contact-form-template' ) ) {
-			        echo '<div class="contact-form-wrap">';
-			        dynamic_sidebar( 'contact-form-template' );
-			        echo '</div>';
-			    }
-
-			    if( is_active_sidebar( 'contact-template' ) ){ 
-			    	echo '<div class="contact-map-wrap">';
-			        dynamic_sidebar( 'contact-template' );
-			        echo '</div>';
-			    }
+			<?php
+			if ( is_active_sidebar( 'contact-form-template-locations' ) ) {
+				echo '<div class="contact-form-template-locations">';
+				dynamic_sidebar( 'contact-form-template-locations' );
+				echo '</div>';
+			}
+			if ( is_active_sidebar( 'contact-form-template' ) ) {
+				echo '<div class="contact-form-wrap">';
+				dynamic_sidebar( 'contact-form-template' );
+				echo '</div>';
+			}
+			if ( is_active_sidebar( 'contact-template' ) ) {
+				echo '<div class="contact-map-wrap">';
+				dynamic_sidebar( 'contact-template' );
+				echo '</div>';
+			}
 			?>
 		</main>
 	</div>	
